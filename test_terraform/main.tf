42ac12a0c1962618a29a925d124e6db1bd8015b9
root <root@ip-172-31-18-1.us-east-2.compute.internal>
2021-09-19 03:36:41 +0000

    Ansible group_vars

--- test_terraform/main.tf
+++ test_terraform/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "devops-2021" {
   instance_type = "t2.micro"
   count = 6
   associate_public_ip_address = true
-  key_name = "rajkey"
+  key_name = "sivakey"
   tags = {
     Name = "pradeep-terraform"
   }
